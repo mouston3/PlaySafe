138a2cb14e8bca32b475502eb0ab2ee4c19816f1
Florent Frossard <florentwm@gmail.com>
2023-04-22 15:03:38 +0200

    Initial Commit

---
+++ Exercice1/Exercice1App.swift
@@ -0,0 +1,17 @@
+//
+//  Exercice1App.swift
+//  Exercice1
+//
+//  Created by test on 22/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct Exercice1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
